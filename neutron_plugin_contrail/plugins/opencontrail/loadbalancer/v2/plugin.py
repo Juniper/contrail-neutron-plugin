63893266c5a63dbafca1d7d8525558fc415acd85
Rudra Rugge <rrugge@juniper.net>
2015-10-07 13:57:01 -0700

    LBAASv2 neutron plugin changes
    First set of LBAASv2 neutron plugin changes.
    
    Change-Id: I04aaeca45e5ab789007cd86a3b9cab0a7ef23a9a
    Partial-Bug: #1503869

---
+++ neutron_plugin_contrail/plugins/opencontrail/loadbalancer/v2/plugin.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+from neutron_lbaas.extensions import loadbalancer
+from loadbalancer_db import LoadBalancerPluginDbV2
+
+
+class LoadBalancerPluginV2(LoadBalancerPluginDbV2):
+    supported_extension_aliases = ["lbaasv2", "extra_lbaas_opts"]
+
+    def __init__(self):
+        super(LoadBalancerPluginV2, self).__init__()
+
+    def get_plugin_description(self):
+        return "OpenContrail LoadBalancerV2 Service Plugin"
+
+    def _pool_update_provider(self, context, pool):
+        if 'provider' not in pool or not pool['provider'] or pool['provider'].__class__ is object:
+            pool['provider'] = "opencontrail"
+
+    def create_pool(self, context, pool):
+        self._pool_update_provider(context, pool['pool'])
+        return super(LoadBalancerPluginV2, self).create_pool(context, pool)
