ec832960563dcc1aca090908327232afea8cc114
Sahil Sabharwal <ssabharwal@juniper.net>
2016-11-17 20:32:08 -0800

    Authentication token url is added in vnc_api
    Contrail neutron plugin expects keystone authentication variables
    which needs to be updated to support latest SKU's from neutron.
    Hence adding auth_token_url in VncApi will rectify this issue.
    
    Changes in contrail neutron plugin to parse and pass auth token
    url to VncApi.
    
    Change-Id: Idea3c8a04ad0935cbf50a70733ec96135744316f
    Closes-Bug: 1639927

--- neutron_plugin_contrail/plugins/opencontrail/loadbalancer/loadbalancer_db.py
+++ neutron_plugin_contrail/plugins/opencontrail/loadbalancer/loadbalancer_db.py
@@ -75,6 +75,11 @@ class LoadBalancerPluginDb(LoadBalancerPluginBase):
         except cfg.NoSuchOptError:
             self.api_server_url = "/"
 
+        try:
+            self.auth_token_url= cfg.CONF.APISERVER.auth_token_url
+        except cfg.NoSuchOptError:
+            self.auth_token_url = None
+
     @property
     def api(self):
         if hasattr(self, '_api'):
@@ -91,7 +96,8 @@ class LoadBalancerPluginDb(LoadBalancerPluginBase):
                         auth_protocol=self.auth_protocol,
                         auth_url=self.auth_url, auth_type=self.auth_type,
                         wait_for_connect=True,
-                        api_server_use_ssl=self.api_srvr_use_ssl)
+                        api_server_use_ssl=self.api_srvr_use_ssl,
+                        auth_token_url=self.auth_token_url)
                 connected = True
             except requests.exceptions.RequestException:
                 time.sleep(3)
