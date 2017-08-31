c4c09c97df02c2a0fafedcc462a8d34c1383b02d
Andrey Shestakov <ashestakov@mirantis.com>
2017-08-31 13:12:05 +0300

    Add functionality to bind/unbind bare metal ports
    Neutron port may contain binding profile, required for baremetal
    provisioning. This change introduce baremetal-vif extension, which
    allows create/update physical and logical interfaces on TORs according
    binding profile.
    
    Change-Id: Iec2ae99369284938e0d463bbc7160fd16c25e077
    Closes-Bug: #1775825

--- neutron_plugin_contrail/plugins/opencontrail/contrail_plugin_base.py
+++ neutron_plugin_contrail/plugins/opencontrail/contrail_plugin_base.py
@@ -59,6 +59,7 @@ from neutron.extensions import portbindings
 from neutron.extensions import securitygroup
 from neutron_plugin_contrail.extensions import serviceinterface
 from neutron_plugin_contrail.extensions import vfbinding
+from neutron_plugin_contrail.extensions import baremetal_vif
 from neutron import neutron_plugin_base_v2
 try:
     from neutron.openstack.common import importutils
@@ -129,7 +130,8 @@ class NeutronPluginContrailCoreBase(neutron_plugin_base_v2.NeutronPluginBaseV2,
                                     portbindings_base.PortBindingBaseMixin,
                                     external_net.External_net,
                                     serviceinterface.Serviceinterface,
-                                    vfbinding.Vfbinding):
+                                    vfbinding.Vfbinding,
+                                    baremetal_vif.BaremetalVIF):
 
     supported_extension_aliases = [
         "security-group",
@@ -380,6 +382,10 @@ class NeutronPluginContrailCoreBase(neutron_plugin_base_v2.NeutronPluginBaseV2,
         """Creates a port on the specified Virtual Network."""
 
         port = self._create_resource('port', context, port)
+
+        if self.is_port_baremetal(port):
+            self.bind_baremetal_port(port)
+
         return port
 
     def get_port(self, context, port_id, fields=None):
@@ -401,6 +407,13 @@ class NeutronPluginContrailCoreBase(neutron_plugin_base_v2.NeutronPluginBaseV2,
                 original['fixed_ips'], port['port']['fixed_ips'])
             port['port']['fixed_ips'] = prev_ips + added_ips
 
+        if self.is_port_baremetal(port['port']):
+            port['port']['id'] = port_id
+            if self.should_bind_port(port['port']):
+                self.bind_baremetal_port(port['port'])
+            else:
+                self.unbind_baremetal_port(port['port'])
+
         return self._update_resource('port', context, port_id, port)
 
     def delete_port(self, context, port_id):
@@ -411,6 +424,9 @@ class NeutronPluginContrailCoreBase(neutron_plugin_base_v2.NeutronPluginBaseV2,
         the remote interface is first un-plugged and then the port
         is deleted.
         """
+        original = self._get_port(context, port_id)
+        if self.is_port_baremetal(original):
+            self.unbind_baremetal_port(original)
 
         self._delete_resource('port', context, port_id)
 
