2eade8b17173a18784c94de72ae9d0a84d73bf76
Édouard Thuleau <ethuleau@juniper.net>
2019-02-20 17:43:42 +0100

    Cleaning and refactoring the code
    - rename the python package to networking-contrail
    - use tox for testing
    - remove deprecated plugin v3 code
    - use Neutron plugin base v2 and extension UT code and skip all failing tests
    - make code pep8 compliant and add gate check
    
    Depends-On: I0925970cd09054fea9921b7e63c8735d383b61b3
    Depends-On: I19894555b56c76fbd504a2a6e9aab67ad5043c28
    Depends-On: Ib825984086a7f7cc2ee3df840e8d7d46bada07ac
    Depends-On: I39fa2083f978b865ea28bc3148bb09580aa5b676
    Depends-On: I3b03a80f7fc25b404d84aa446db0b8388d867b5c
    Change-Id: Ie04e63d4af156163df901856554c62b81c55ae26
    Closes-Jira-Bug: JCB-219347

---
+++ networking_contrail/extensions/loadbalancercustomattributes.py
@@ -0,0 +1,75 @@
+try:
+    from neutron.api.extensions import ExtensionDescriptor
+except ImportError:
+    from neutron_lib.api.extensions import ExtensionDescriptor
+try:
+    from neutron.api.v2.attributes import ATTR_NOT_SPECIFIED
+except ImportError:
+    from neutron_lib.constants import ATTR_NOT_SPECIFIED
+
+
+def _validate_custom_attributes(data, valid_values=None):
+    if not isinstance(data, list):
+        msg = ("Invalid data format for custom_attributes: '%s'") % data
+        return msg
+
+
+def convert_none_to_empty_list(value):
+    return [] if value is None else value
+
+
+try:
+    from neutron.api.v2 import attributes as attrs
+    if hasattr(attrs, 'validators'):
+        attrs.validators['type:customattributes'] = _validate_custom_attributes
+    else:
+        from neutron_lib.api import validators
+        validators.add_validator('type:customattributes',
+                                 _validate_custom_attributes)
+except ImportError:
+    from neutron_lib.api import validators
+    validators.add_validator('type:customattributes',
+                             _validate_custom_attributes)
+
+
+# Extended_Attribute MAP
+EXTENDED_ATTRIBUTES_2_0 = {
+    'pools': {
+        'custom_attributes': {'allow_post': True, 'allow_put': True,
+                              'convert_to': convert_none_to_empty_list,
+                              'default': ATTR_NOT_SPECIFIED,
+                              'validate': {'type:customattributes': None},
+                              'is_visible': True},
+    }
+}
+
+
+class Loadbalancercustomattributes(ExtensionDescriptor):
+
+    @classmethod
+    def get_name(cls):
+        return "Loadbalancer as a Service"
+
+    @classmethod
+    def get_alias(cls):
+        return "extra_lbaas_opts"
+
+    @classmethod
+    def get_description(cls):
+        return "Custom LBaaS attributes"
+
+    @classmethod
+    def get_namespace(cls):
+        return "http://docs.openstack.org/TODO"
+
+    @classmethod
+    def get_updated(cls):
+        return "2015-07-17T15:00:00-00:00"
+
+    @classmethod
+    def get_extended_resources(self, version):
+        """Return Ext Resources."""
+        if version == "2.0":
+            return EXTENDED_ATTRIBUTES_2_0
+        else:
+            return {}
