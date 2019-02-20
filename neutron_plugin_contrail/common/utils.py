5e0ecfaf4d65b0443bb38a6deade6b3b859b8e6e
Édouard Thuleau <ethuleau@juniper.net>
2019-02-20 17:43:42 +0100

    Cleaning and refactoring the code
    - Does NOT rename the python package to networking-contrail for convenience
    - use tox for testing
    - remove deprecated plugin v3 code
    - use Neutron plugin base v2 and extension UT code and skip all failing tests
    - make code pep8 compliant and add gate check
    
    Depends-On: I19894555b56c76fbd504a2a6e9aab67ad5043c28
    Depends-On: Ib825984086a7f7cc2ee3df840e8d7d46bada07ac
    Depends-On: Ie2e64f8040222e0a03bb5cf6f144a2413b697a53
    Depends-On: I3b03a80f7fc25b404d84aa446db0b8388d867b5c
    Change-Id: Ie04e63d4af156163df901856554c62b81c55ae26
    Closes-Jira-Bug: JCB-219347

--- neutron_plugin_contrail/common/utils.py
+++ neutron_plugin_contrail/common/utils.py
@@ -12,14 +12,15 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import requests
 from urlparse import urlparse
 
 from oslo_config import cfg
+import requests
 from vnc_api.vnc_api import VncApi
 
-import constants
-import exceptions as c_exc
+from neutron_plugin_contrail.common import constants
+from neutron_plugin_contrail.common import exceptions as c_exc
+
 
 vnc_opts = [
     cfg.StrOpt('api_server_ip',
@@ -32,9 +33,7 @@ vnc_opts = [
                default=constants.VNC_API_DEFAULT_BASE_URL,
                help='URL path to request VNC API'),
     cfg.DictOpt('contrail_extensions',
-                default={'contrail': None,
-                         'service-interface': None,
-                         'vf-binding': None},
+                default={},
                 help='Enable Contrail extensions (default: %(default)s)'),
     cfg.BoolOpt('use_ssl',
                 default=constants.VNC_API_DEFAULT_USE_SSL,
@@ -54,28 +53,6 @@ vnc_opts = [
                     'configuration section.'),
 ]
 
-analytics_opts = [
-    cfg.StrOpt('analytics_api_ip',
-               default=constants.ANALYTICS_API_DEFAULT_HOST,
-               help='IP address to connect to Analytics API'),
-    cfg.IntOpt('analytics_api_port',
-               default=constants.ANALYTICS_API_DEFAULT_PORT,
-               help='Port to connect to Analytics API'),
-]
-
-vrouter_opts = [
-    cfg.StrOpt('vhostuser_sockets_dir',
-               default='/var/run/vrouter',
-               help='Path to dir where vhostuser socket are placed'),
-]
-
-vnc_extra_opts = [
-    cfg.BoolOpt('apply_subnet_host_routes',
-                default=False,
-                help="Apply Neutron subnet host routes to Contrail virtual "
-                     "network with a route table"),
-]
-
 
 class RoundRobinApiServers(object):
     def __init__(self):
@@ -90,23 +67,18 @@ class RoundRobinApiServers(object):
             self.index = 0
         return self.api_servers[self.index]
 
-    def len(self):
+    def length(self):
         return len(self.api_servers)
 
+
 def register_vnc_api_options():
-    """Register Contrail Neutron core plugin configuration flags"""
-    cfg.CONF.register_opts(vnc_opts, 'APISERVER')
-    cfg.CONF.register_opts(analytics_opts, 'COLLECTOR')
-    cfg.CONF.register_opts(vrouter_opts, 'VROUTER')
-
-
-def register_vnc_api_extra_options():
-    """Register extra Contrail Neutron core plugin configuration flags"""
-    cfg.CONF.register_opts(vnc_extra_opts, 'APISERVER')
+    """Register Contrail Neutron core plugin configuration flags."""
+    if 'APISERVER' not in cfg.CONF:
+        cfg.CONF.register_opts(vnc_opts, 'APISERVER')
 
 
 def vnc_api_is_authenticated(api_server_ip):
-    """ Determines if the VNC API needs credentials
+    """Determine if the VNC API needs credentials.
 
     :returns: True if credentials are needed, False otherwise
     """
@@ -115,7 +87,8 @@ def vnc_api_is_authenticated(api_server_ip):
         api_server_ip,
         cfg.CONF.APISERVER.api_server_port
     )
-    response = requests.get(url, verify=cfg.CONF.APISERVER.get('cafile', False))
+    response = requests.get(url,
+                            verify=cfg.CONF.APISERVER.get('cafile', False))
 
     if response.status_code == requests.codes.ok:
         return False
@@ -143,7 +116,7 @@ def get_keystone_auth_info():
 
 
 def get_vnc_api_instance(wait_for_connect=True):
-    """ Instantiates a VncApi object from configured parameters
+    """Instantiate a VncApi object from configured parameters.
 
     Read all necessary configuration options from neutron and contrail core
     plugin configuration files and instantiates a VncApi object with them.
