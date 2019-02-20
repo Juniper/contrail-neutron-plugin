e3f056b66aef5778f1e9dc5c2f3abac2ea8b2427
Édouard Thuleau <ethuleau@juniper.net>
2019-02-20 17:43:42 +0100

    Cleaning and refactoring the code
    - Does NOT rename the python package to networking-contrail for convenience
    - use tox for testing
    - remove deprecated plugin v3 code
    - use Neutron plugin base v2 and extension UT code and skip all failing tests
    - make code pep8 compliant and add gate check
    
    Depends-On: Ib825984086a7f7cc2ee3df840e8d7d46bada07ac
    Depends-On: Ie2e64f8040222e0a03bb5cf6f144a2413b697a53
    Change-Id: Ie04e63d4af156163df901856554c62b81c55ae26
    Closes-Jira-Bug: JCB-219347

--- neutron_plugin_contrail/common/utils.py
+++ neutron_plugin_contrail/common/utils.py
@@ -12,21 +12,21 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_config import cfg
+try:
+    from neutron.openstack.common import log as logging
+except ImportError:
+    from oslo_log import log as logging
 import requests
 from six.moves.urllib.parse import urlparse
-
-from oslo_config import cfg
 from vnc_api.vnc_api import VncApi
 
 from neutron_plugin_contrail.common import constants
 from neutron_plugin_contrail.common import exceptions as c_exc
 
-try:
-    from neutron.openstack.common import log as logging
-except ImportError:
-    from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
+
 
 vnc_opts = [
     cfg.StrOpt('api_server_ip',
@@ -39,9 +39,7 @@ vnc_opts = [
                default=constants.VNC_API_DEFAULT_BASE_URL,
                help='URL path to request VNC API'),
     cfg.DictOpt('contrail_extensions',
-                default={'contrail': None,
-                         'service-interface': None,
-                         'vf-binding': None},
+                default={},
                 help='Enable Contrail extensions (default: %(default)s)'),
     cfg.BoolOpt('use_ssl',
                 default=constants.VNC_API_DEFAULT_USE_SSL,
@@ -67,26 +65,13 @@ vnc_opts = [
                help='VNC API Server connection timeout in seconds'),
 ]
 
-vrouter_opts = [
-    cfg.StrOpt('vhostuser_sockets_dir',
-               default='/var/run/vrouter',
-               help='Path to dir where vhostuser socket are placed'),
-]
-
-vnc_extra_opts = [
-    cfg.BoolOpt('apply_subnet_host_routes',
-                default=False,
-                help="Apply Neutron subnet host routes to Contrail virtual "
-                     "network with a route table"),
-]
-
 
 class RoundRobinApiServers(object):
     def __init__(self):
         self.api_servers = cfg.CONF.APISERVER.api_server_ip.split()
         self.index = -1
 
-    def get(self,api_servers):
+    def get(self, api_servers):
         # use the next host in the list
         self.index += 1
         if self.index >= len(api_servers):
@@ -94,22 +79,19 @@ class RoundRobinApiServers(object):
             self.index = 0
         return api_servers[self.index]
 
-    def len(self):
+    def __len__(self):
+        """Return API servers list length."""
         return len(self.api_servers)
 
+
 def register_vnc_api_options():
-    """Register Contrail Neutron core plugin configuration flags"""
-    cfg.CONF.register_opts(vnc_opts, 'APISERVER')
-    cfg.CONF.register_opts(vrouter_opts, 'VROUTER')
-
-
-def register_vnc_api_extra_options():
-    """Register extra Contrail Neutron core plugin configuration flags"""
-    cfg.CONF.register_opts(vnc_extra_opts, 'APISERVER')
+    """Register Contrail Neutron core plugin configuration flags."""
+    if 'APISERVER' not in cfg.CONF:
+        cfg.CONF.register_opts(vnc_opts, 'APISERVER')
 
 
 def vnc_api_is_authenticated(api_server_ips):
-    """ Determines if the VNC API needs credentials
+    """Determine if the VNC API needs credentials.
 
     :returns: True if credentials are needed, False otherwise
     """
@@ -121,9 +103,10 @@ def vnc_api_is_authenticated(api_server_ips):
         )
 
         try:
-            response = requests.get(url,
+            response = requests.get(
+                url,
                 timeout=(cfg.CONF.APISERVER.connection_timeout,
-                            cfg.CONF.APISERVER.timeout),
+                         cfg.CONF.APISERVER.timeout),
                 verify=cfg.CONF.APISERVER.get('cafile', False))
         except requests.exceptions.RequestException as e:
             LOG.warning("Failed connecting to API server: %s" % e)
@@ -158,7 +141,7 @@ def get_keystone_auth_info():
 
 
 def get_vnc_api_instance(wait_for_connect=True):
-    """ Instantiates a VncApi object from configured parameters
+    """Instantiate a VncApi object from configured parameters.
 
     Read all necessary configuration options from neutron and contrail core
     plugin configuration files and instantiates a VncApi object with them.
