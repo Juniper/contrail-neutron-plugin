536ebcb8a876db983c7d0ce1be11a244b6455fd3
Édouard Thuleau <ethuleau@juniper.net>
2019-02-20 17:43:42 +0100

    Cleaning and refactoring the code
    - Does NOT rename the python package to networking-contrail for convenience
    - use tox for testing
    - remove deprecated plugin v3 code
    - use Neutron plugin base v2 and extension UT code and skip all failing tests
    - make code pep8 compliant and add gate check
    
    Depends-On: I19894555b56c76fbd504a2a6e9aab67ad5043c28
    Depends-On: Ib825984086a7f7cc2ee3df840e8d7d46bada07ac
    Depends-On: Ie2e64f8040222e0a03bb5cf6f144a2413b697a53
    Change-Id: Ie04e63d4af156163df901856554c62b81c55ae26
    Closes-Jira-Bug: CEM-10765

--- setup.py
+++ setup.py
@@ -2,33 +2,53 @@
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
 
-from setuptools import setup, find_packages
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
 
 def requirements(filename):
     with open(filename) as f:
         lines = f.read().splitlines()
-    return lines
+    c = re.compile(r'\s*#.*')
+    return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
+
 
 setup(
     name='neutron_plugin_contrail',
     version='0.1dev',
+    long_description=open('README.rst').read(),
     packages=find_packages(),
-    package_data={'': ['*.html', '*.css', '*.xml']},
-    zip_safe=False,
-    long_description="Contrail neutron plugin",
-
     install_requires=requirements('requirements.txt'),
-
-    test_suite='neutron_plugin_contrail.tests',
     tests_require=requirements('test-requirements.txt'),
-
+    author_email='dev@lists.opencontrail.org',
+    url='https://github.com/Juniper/contrail-neutron-plugin',
+    classifier=[
+        'Environment :: OpenStack',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
     entry_points={
-        'neutron.service_plugins': [
-            'contrail-timestamp = neutron_plugin_contrail.plugins.opencontrail.services.timestamp.timestamp_plugin:TimeStampPlugin',
-            'contrail-trunk = neutron_plugin_contrail.plugins.opencontrail.services.trunk.plugin:TrunkPlugin',
+        'neutron.core_plugins': [
+            'contrail = neutron_plugin_contrail.plugin.plugin:ContrailPlugin',
+            # for backward compatibility with old plugin structure:
+            'neutron_plugin_contrail.plugins.opencontrail.contrail_plugin.NeutronPluginContrailCoreV2 = neutron_plugin_contrail.plugin.plugin:ContrailPlugin',
         ],
-        'firewall_drivers': [
-            'contrail-fwaasv2 = neutron_plugin_contrail.plugins.opencontrail.neutron_fwaas.contrail:ContrailFirewallv2Driver',
+        'neutron.service_plugins': [
+            'contrail-timestamp = neutron_plugin_contrail.services.timestamp.timestamp_plugin:TimeStampPlugin',
+            'contrail-lbaasv2 = neutron_plugin_contrail.services.loadbalancer.v2.plugin:LoadBalancerPluginV2',
+            # for backward compatibility with old plugin structure:
+            'neutron_plugin_contrail.plugins.opencontrail.loadbalancer.v2.plugin.LoadBalancerPluginV2 = neutron_plugin_contrail.services.loadbalancer.v2.plugin:LoadBalancerPluginV2',
+            'contrail-trunk = neutron_plugin_contrail.services.trunk.plugin:TrunkPlugin',
         ],
     },
+    test_suite=' neutron_plugin_contrail.tests',
 )
